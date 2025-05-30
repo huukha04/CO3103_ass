f1f3896aeeaeb54a9ac17e67af1932fbe015dc8b
Vo Phuong <maiphuongbrt9a1@gmail.com>
2025-05-30 19:26:04 +0700

    merge banch FE to maiphuongFE to get to code

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,9 +7,8 @@ export default {
         roboto: ["Roboto", "sans-serif"],
         sans: ["Poppins", "sans-serif"],
         serif: ["Merriweather", "serif"],
-        jakarta: ['Plus Jakarta Sans','serif'],
-        poppins: ['Poppins', 'sans-serif'],
-
+        jakarta: ["Plus Jakarta Sans", "serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
       boxShadow: {
         "header-bottom": "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
@@ -21,7 +20,10 @@ export default {
       colors: {
         gray: "#5A637A",
         lightgray: "#8a8a8a",
-        pink: "#FFB5BF",
+
+        // xóa cái định nghĩa màu pink ở đây do nó định nghĩa lại màu pink
+        // làm cho tailwind không nhận diện được các mã màu được cài đặt ăn theo màu pink là text-pink-500, bg-pink-500, border-pink-500
+        // pink: "#FFB5BF",
         coral: "#FF6969",
         hover: "#AD4042",
         darkblue: "#24305E",
